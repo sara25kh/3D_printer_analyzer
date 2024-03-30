489cf62eb899cadbbaa812938c488f86e4547091
Sara Khatir <sara25_kh@aut.ac.ir>
2024-03-31 02:40:30 +0330

    Implement Gcode generator for the simple wall

---
+++ src/GCodeGenerator.py
@@ -0,0 +1,42 @@
+class GCodeGenerator:
+    def __init__(self):
+        self.commands = []
+        self.extrude_rate = 1
+        self.total_extrude = 0
+        self.default_speed = 1000
+        self.ending_commands = []
+        self.layer_height = 0.2
+        self.total_z = 0.2
+        self.last_position = (0, 0)
+
+    def set_extrude_rate(self, rate):
+        self.extrude_rate = rate
+    
+    def set_default_speed(self, speed):
+        self.default_speed = speed
+
+    def set_layer_height(self, height):
+        self.layer_height = height
+
+    def move(self, x, y, speed = None):
+        self.commands.append(f"G0 X{x} Y{y} F{speed or self.default_speed}")
+        self.last_position = (x, y)
+
+    def move_and_extrude(self, x, y, speed = None):
+        dist = ((x - self.last_position[0]) ** 2 + (y - self.last_position[1]) ** 2) ** 0.5
+        self.total_extrude += self.extrude_rate * dist
+        self.commands.append(f"G0 X{x} Y{y} F{speed or self.default_speed} E{self.total_extrude}")
+
+    def go_to_next_layer(self):
+        self.total_z += self.layer_height
+        self.total_z = round(self.total_z, 4)  # Update the precision to 4 digits
+        self.commands.append(f"G0 Z{self.total_z:.4f}")  # Print with 4 digits of precision
+
+    def set_start_gcode_list(self, start_gcode_list):
+        self.commands = start_gcode_list + self.commands
+    
+    def set_end_gcode_list(self, end_gcode_list):
+        self.ending_commands = end_gcode_list
+
+    def generate(self):
+        return self.commands + self.ending_commands
---
+++ src/SimpleWall.py
@@ -0,0 +1,52 @@
+
+from GCodeGenerator import GCodeGenerator
+
+class WallPrinter:
+    def __init__(self, p1, p2, height):
+        self.p1 = p1
+        self.p2 = p2
+        self.height = height
+
+    def generate_gcode(self):
+        gcode_generator = GCodeGenerator()
+        gcode_generator.set_start_gcode_list([
+            # 'M104 S150                                    ; start heating the extruder a bit to save some time but prevent oozing',
+            # 'M140 S[first_layer_bed_temperature]          ; heatbed temperature',
+            # 'M190 S[first_layer_bed_temperature]          ; wait for the bed to heat up',
+            # 'M83                                          ; extruder relative mode',
+            'G28                                          ; home all axes',
+            # 'G29                                          ; Bed autolevel (optional, for BLTouch only)',
+            'G92 E0                                       ; reset extruder',
+            # 'G1 X0 Y0 F5000                               ; move to 0/0/0',
+            'G1 Z0.2',
+            # 'M109 S[first_layer_temperature]              ; Heat up extruder',
+            # 'M42 P4 S0                                    ; Turn off LED (optional)',
+            # 'M42 P5 S0',
+            # 'M42 P6 S0',
+            # 'G1 X20 Y5 Z0.3 F5000.0                       ; move to start-line position',
+            # 'G1 Z0.3 F1000                                ; print height',
+            # 'G1 X200 Y5 F1500.0 E15                       ; draw 1st line',
+            # 'G1 X200 Y5.3 Z0.3 F5000.0                    ; move to side a little',
+            # 'G1 X5.3  Y5.3 Z0.3 F1500.0 E30               ; draw 2nd line'
+        ])
+        gcode_generator.move(self.p1[0], self.p1[1], 0)
+        gcode_generator.set_extrude_rate(0)
+
+        # Move to the starting position
+        gcode_generator.move(self.p1[0], self.p1[1])
+
+        # Generate GCode for each layer
+        while gcode_generator.total_z < self.height:
+            gcode_generator.move_and_extrude(self.p2[0], self.p2[1])
+            gcode_generator.go_to_next_layer()
+            gcode_generator.move_and_extrude(self.p1[0], self.p1[1])
+            gcode_generator.go_to_next_layer()
+
+        return gcode_generator.generate()
+
+
+
+# Run test that prints the generated gcode of this class:
+if __name__ == '__main__':
+    wall_printer = WallPrinter((0, 0), (100, 0), 10)
+    print(wall_printer.generate_gcode())
